fc1ebeff6a0ebe90723ac299144a48097f45451e
AnnKraken <annarats99@gmail.com>
2024-05-28 12:17:28 +0300

    start prila

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-"use strict";
+'use strict';
 
-let decr = 10;
-decr --;
-console.log(decr);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
